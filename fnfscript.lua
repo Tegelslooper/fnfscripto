733172392664dc016156227f586100a54975abf7
Tegelslooper <81248048+Tegelslooper@users.noreply.github.com>
2021-03-23 16:42:10 +0100

    Update fnfscript.lua

--- fnfscript.lua
+++ fnfscript.lua
@@ -27,8 +27,8 @@ do
     Variables.NoteAccuracy = {}
 
     Variables.Constants.DISTANCE_TO_ACCURACY = {
-        ["Sick"] = 0.03,
-        ["Good"] = 0.08,
+        ["Sick"] = 0.02,
+        ["Good"] = 0.07,
         ["Ok"] = 0.13,
         ["Bad"] = 0.2
     }
